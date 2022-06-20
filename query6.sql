fcefbb498ab46ce6d852c7bbfd25bd9e9555a2f7
Nidhi Gokani <nidhi.gokani@pc0383.local>
2022-06-20 12:59:46 +0530

    commit

--- query6.sql
+++ query6.sql
@@ -15,3 +15,34 @@ INNER JOIN incentive on employees.eId=incentive.eId
 WHERE incamount>3000
 
 SELECT * from incGrt
+
+-- (Note refer tables from sql supported files) 
+-- Create a View to Find the names (first_name, last_name), job, department number, and department name of the employees who work in London 
+CREATE VIEW workAt as
+SELECT fName||" "||lName as names, job_id,employee.dep_id,dep_name FROM employee 
+INNER JOIN department on employee.emp_id=department.dep_id
+WHERE employee.c_id=(SELECT country_id from countries where country_name='London')
+
+SELECT * from workAt
+
+-- Create a View to get the department name and number of employees in the department. 
+CREATE VIEW depData as
+SELECT department.dep_name,count(*) FROM employee 
+INNER JOIN department on employee.dep_id=department.dep_id
+GROUP by dep_name
+
+SELECT * from depData
+
+-- Find the employee ID, job title, number of days between ending date and starting date for all jobs in department 90 from job history.
+SELECT EmployeeId,JobHistory.Job_Id,job_title,
+	(JULIANDAY(EndDate)-JULIANDAY(StartDate)) AS date_difference 
+	FROM JobHistory 
+	INNER JOIN jobs on JobHistory.Job_Id=jobs.job_id
+	WHERE JobHistory.Job_Id=1
+	
+
+    
+
+-- Write a View to display the department name, manager name, and city. 
+
+-- Create a View to display department name, name (first_name, last_name), hire date, salary of the manager for all managers whose experience is more than 15 years.
